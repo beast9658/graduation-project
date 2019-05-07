5dcc08d868654700b589d673d0167412c611429c
unknown <1577864192@qq.com>
2019-05-07 17:00:42 +0800

    header-init

--- vue-front/src/App.vue
+++ vue-front/src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <Header></Header>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/commonHeader'
+import Footer from './components/commonFooter'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Header,
+    Footer
   }
 }
 </script>
@@ -23,6 +28,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+* {
+  padding: 0;
+  margin: 0;
 }
 </style>
---
+++ vue-front/src/components/commonFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    <div class="wrapper">
+      <p>5555555555555555555555</p>
+      <p>5555555555555555555555</p>
+      <p>5555555555555555555555</p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.wrapper {
+  position: fixed;
+  bottom: 0;
+  text-align: auto;
+  background-color: aqua;
+}
+</style>
