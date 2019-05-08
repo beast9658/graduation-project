b166a962740063cac55188c8a2f9e9975b25be6f
unknown <1577864192@qq.com>
2019-05-08 10:30:22 +0800

    init

--- vue-front/src/App.vue
+++ vue-front/src/App.vue
@@ -1,27 +1,14 @@
 <template>
   <div id="app">
-    <Header></Header>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <Footer></Footer>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-import Header from './components/commonHeader'
-import Footer from './components/commonFooter'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-    Header,
-    Footer
-  }
-}
-</script>
-
-<style>
+<style lang="less">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -29,8 +16,14 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
-* {
-  padding: 0;
-  margin: 0;
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
--- vue-front/src/main.js
+++ vue-front/src/main.js
@@ -1,13 +1,13 @@
-import Vue from 'vue';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import App from './App.vue';
-
-Vue.use(ElementUI);
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
 new Vue({
-  el: '#app',
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
